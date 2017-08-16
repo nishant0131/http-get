d979875b599c5419277a0789a3524d39cac1db59
cynoteck <cynoteck@cynodoon.com>
2017-08-16 18:56:01 +0530

    navigation and adddfull-width slider

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import * as $ from 'jquery';
+import './slider.js';  
 
 @Component({
   selector: 'app-services',
--- src/app/services/slider.js
+++ src/app/services/slider.js
@@ -1,42 +1,54 @@
-alert('sdfvv');
-var main = function() {
-
-	var paused = false
-
-	$('.arrowR').click(function() {
-		paused = true;
-		$('#slideshow > div:first')
-		.fadeOut(1000)
-		.next()
-		.fadeIn(1000)
-		.end()
-		.appendTo('#slideshow');
-	});
-		
-	$('.arrowL').click(function() {
-		paused = true;
-		$('#slideshow > div:last')
-		.fadeIn(1000)
-		.prependTo('#slideshow')
-		.next()
-		.fadeOut(1000)
-		.end();
-	});
-
-
+;(function() {
 	
-	setInterval(function() {
-		if (paused === false) { 
-			$('#slideshow > div:first')
-			.fadeOut(1000)
-			.next()
-			.fadeIn(1000)
-			.end()
-			.appendTo('#slideshow');
-		};
-	},  5000);
-   
+	  $(document).ready(function() {
 	
-};
-
-$(document).ready(main);
+		var slider = function() {
+		  
+		  var backImg = [];
+		  backImg[0] = "http://www.matejmichalik.com/black-and-white-photography/galleries/post-427/full/Black-and-white-photo-Tanah-lot-bali.jpg";
+		  backImg[1] = "http://cdn.digital-photo-secrets.com/images/black-white-dendelion-large.jpg";
+		  backImg[2] = "http://picturescollections.com/wp-content/uploads/2012/04/Black_And_White_Pictures...jpg";
+		  
+		  var i = 0;
+		  var x = (backImg.length) - 1;
+		  var int = 3500;
+		  
+		  var initialize = function() {
+			attachEvents();
+		  };
+		  
+		  var attachEvents = function() {
+			$('.btnLeft').click(function() {
+			  left();
+			});
+	
+			$('.btnRight').click(function() {
+			  right();
+			});
+		  };
+	
+		  var changeImg = function() {
+			$('.slider').css('background-image', 'url(' + backImg[i] + ')');
+		  }
+	
+		  var left = function() {
+			(i <= 0) ? i = 3 : i--;
+			changeImg(i);
+		  };
+	
+		  var right = function() {
+			(i >= x) ? i = 0 : i++;
+			changeImg(i);
+		  };
+	
+		  window.setInterval(right, int);
+	
+		  changeImg(i);
+		  return initialize();
+		}
+	
+		slider();
+	
+	  });
+	  
+	})();
